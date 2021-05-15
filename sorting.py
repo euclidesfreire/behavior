86de6bf5f40ce011b44802d015549c0ecdb7b25c
Euclides Freire <euclidesfreiresa@gmail.com>
2021-05-15 19:06:08 -0300

    Create Algoritmo Merge Sort in Sorting

--- sorting.py
+++ sorting.py
@@ -1,3 +1,12 @@
+#
+# Insertion Sorting 
+#
+# @param 
+# A - Array
+#
+# @return
+# A - Sorting Array
+#
 def insertion_sort(A):
 
     for j in range(1,len(A)):  
@@ -11,3 +20,57 @@ def insertion_sort(A):
         A[i+1] = key
     
     return A
+
+#
+# Merge Sorting 
+#
+# @param 
+# A - Array
+# p - Array Start
+# r - Array End
+#
+# @return
+# A - Sorting Array
+#
+def merge_sort(A, p, r):
+    # p <= q < r
+    q = (p+r) // 2
+
+    if(p == r):
+        return
+    
+    merge_sort(A, p, q)
+    merge_sort(A, q+1, r)
+
+    return _merge(A, p, q, r)
+
+
+def _merge(A, p, q, r):
+    #Output Array 
+    B = []
+
+    i = p
+    j = q+1
+
+    while True:
+        if(A[i] <= A[j]):
+            B.append(A[i])
+            
+            if(i == q):
+                B.extend(A[j:r+1]) #Inserir a segunda pilha em B
+                break
+
+            i += 1
+        else:
+            B.append(A[j])
+
+            if(j == r):
+                B.extend(A[i:q+1]) #Inserir a primeira pilha em B
+                break
+
+            j += 1
+    
+    A[p:r+1] = B
+
+    return A
+
